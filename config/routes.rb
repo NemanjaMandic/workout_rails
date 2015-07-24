2214248b888f70830f4100bec8adef50060808e3
nemanjamandic <nemanjans84@gmail.com>
2015-07-24 22:59:51 +0000

    Ad app gems, and finished forms

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   
+  get 'pages/index'
+
+  get 'pages/about'
+
+  get 'pages/galery'
+
+  get 'pages/contact'
+
   resources :workouts
   root "workouts#index"
 end
